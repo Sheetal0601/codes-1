60bf78e9d3082e91200bf7e63d98afaf8581d963
Ashutosh Gautam <75945709+Traitor000@users.noreply.github.com>
2021-10-03 23:51:44 +0530

    Solved complex algorithms problems in less time
    Please merge optimized solutions

---
+++ 1.weirdAlgorithm.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+#define ll long long int
+#define quick ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+void sol()
+{
+   ll n;
+    std::cin>>n;
+ 
+  cout<<n<<" ";
+  while(n!=1){
+    
+    if(n&1==1)
+    {n=3*n+1;
+      std::cout<<n<<" ";
+      
+    }
+    else
+    {
+      n=n/2;
+     std::cout<<n<<" ";
+    }
+  }
+  
+}   
+
+int main()
+{
+      quick
+   
+      
+  
+           sol();
+     return 0;
+}
