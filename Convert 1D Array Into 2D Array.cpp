b13257eef1deafda46f46b5915739ae1ba0532c9
Sheetal Lalwani <68683093+Sheetal0601@users.noreply.github.com>
2021-10-05 18:13:38 +0530

    Create Convert 1D Array Into 2D Array.cpp

---
+++ Convert 1D Array Into 2D Array.cpp
@@ -0,0 +1,11 @@
+link : https://leetcode.com/problems/convert-1d-array-into-2d-array/
+vector<vector<int>> construct2DArray(vector<int>& a, int m, int n) {
+        int l=a.size();
+        if(m*n < l or m*n > l) return {};
+        vector<vector<int>> ans(m, vector<int> (n,0));
+        for(int i=0; i<l; i++)
+        {
+            ans[i/n][i%n]=a[i];
+        }
+        return ans;
+    }
